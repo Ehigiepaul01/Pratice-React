331e9541f8e49666cf044eb0104869af9dd642d1
Ehigiepaul01 <paulehigie5@gmail.com>
2023-07-02 14:26:50 +0100

    tried creating react component

--- praticeapp/src/App.js
+++ praticeapp/src/App.js
@@ -1,19 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from './Heading'
 
-function Heading() { 
-  return ( 
-    <h1>This is my First React App.</h1> 
-  ) 
-} 
- 
-function App() { 
-  return ( 
-    <div className="App"> 
-      Welcome to my first React App, looking forward to doing many more great things 
-      <Heading /> 
-    </div> 
-  ); 
-} 
- 
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
 export default App;
