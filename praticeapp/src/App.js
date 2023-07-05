c7572809bba1790fd7faf99a6345607ba582b208
Ehigiepaul01 <paulehigie5@gmail.com>
2023-07-05 15:43:29 +0100

    more update made

--- praticeapp/src/App.js
+++ praticeapp/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <Heading firstName="Paul" />
       <Heading firstName="My full name is Paul Oluwapelumi Ehigie" />
+      <Heading firstName="I will be changing the world for good" />
     </div>
   );
 }
