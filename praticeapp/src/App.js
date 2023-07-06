557b5cf1f8569f8e97ba203d5b36338bfedeed66
Ehigiepaul01 <paulehigie5@gmail.com>
2023-07-06 16:28:01 +0100

    praticed more of components

--- praticeapp/src/App.js
+++ praticeapp/src/App.js
@@ -1,13 +1,25 @@
-import Heading from "./Heading";
+// import Heading from "./Heading";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Heading firstName="Paul" />
+//       <Heading firstName="My full name is Paul Oluwapelumi Ehigie" />
+//       <Heading firstName="I will be changing the world for good" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Heading firstName="Paul" />
-      <Heading firstName="My full name is Paul Oluwapelumi Ehigie" />
-      <Heading firstName="I will be changing the world for good" />
-    </div>
-  );
+  return (
+    <div>
+      <h1>Task: Add three Card elements</h1>
+    </div>
+  );
 }
 
 export default App;
